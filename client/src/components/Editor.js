72cfd8471b95119376ca074384431e9a88f77079
Eric Zhang <ekzhang1@gmail.com>
2019-01-30 19:21:58 -0600

    Update template

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -11,14 +11,11 @@ using namespace std;
 typedef long long LL;
 const double PI = 4 * atan(1);
 
-#define MAXN 100013
-int N;
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> N;
+    // your code here
 
     cout.flush();
     return 0;
