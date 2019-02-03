0690d567aada952428aa8f4f3c3f7abf98f6d68d
Eric Zhang <ekzhang1@gmail.com>
2019-02-03 13:03:40 -0600

    Add recent workspaces to index page

---
+++ client/src/js/languages.js
@@ -0,0 +1,22 @@
+export default {
+  4: {
+    name: 'C',
+    mode: 'c_cpp'
+  },
+  10: {
+    name: 'C++',
+    mode: 'c_cpp'
+  },
+  26: {
+    name: 'Java',
+    mode: 'java'
+  },
+  34: {
+    name: 'Python 3',
+    mode: 'python'
+  },
+  38: {
+    name: 'Ruby',
+    mode: 'ruby'
+  }
+}
--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { api } from '../js/api';
+import languages from '../js/languages';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-function IndexPage() {
-  return (
-    <div className="p-4">
-      <h1>Welcome!</h1>
-    </div>
-  );
+class IndexPage extends Component {
+  state = { recent: null };
+
+  async componentDidMount() {
+    const { data } = await api.get('/workspace');
+    this.setState({ recent: data });
+  }
+
+  render() {
+    return (
+      <div className="p-4 h-100" style={{ overflowY: 'auto' }}>
+        <h1>Welcome!</h1>
+        <div className="d-flex flex-wrap">
+          {this.state.recent && this.state.recent.map(workspace =>
+            <Card key={workspace._id} className="m-2" style={{ flexBasis: '250px' }}>
+              <CardBody>
+                <CardTitle className="font-weight-bold">{workspace.problem.title}</CardTitle>
+                <CardSubtitle className="font-italic">{moment(workspace.updatedAt).fromNow()}</CardSubtitle>
+                <CardText>{workspace.solution && (languages[workspace.solution.language] || {}).name}</CardText>
+                <Link to={`/workspace/${workspace._id}`}><Button>Open</Button></Link>
+              </CardBody>
+            </Card>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default IndexPage;
